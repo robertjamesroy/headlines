53d2a78e8c808b4357e08479984238dfc20d5804
Rob Roy <robertjamesroy@outlook.com>
2023-10-26 17:40:29 -0400

    Using GET

--- headlines.py
+++ headlines.py
@@ -1,5 +1,5 @@
 import feedparser
-
+from flask import request
 from flask import Flask
 from flask import render_template
 
@@ -14,24 +14,36 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
  'TRSecurity' : 'https://www.techrepublic.com/rssfeeds/topic/security/',
  'WiredSecurity' : 'https://www.wired.com/feed/tag/wired-guide/latest/rss',
  'TCSecurity' : 'https://techcrunch.com/category/security/feed/'}
-#@app.route("/")
 
-@app.route("/")
-@app.route("/<publication>")
-def get_news(publication="bbc"):
- feed = feedparser.parse(RSS_FEEDS[publication])
- #first_article = feed['entries'][0]
- return render_template("home.html",  articles=feed['entries'])
+
+# @app.route("/")
+# @app.route("/<publication>")
+# def get_news(publication="bbc"):
+#  feed = feedparser.parse(RSS_FEEDS[publication])
+#  first_article = feed['entries'][0]
+#  return render_template("home.html",  articles=feed['entries'])
 #  title=first_article.get("title"),
 #  published=first_article.get("published"),
 #  summary=first_article.get("summary"))
  #return render_template("home.html")
-@app.route("/bbc")
-def bbc():
- return get_news('bbc')
-@app.route("/cnn")
-def cnn():
- return get_news('cnn')
+
+@app.route("/")
+def get_news():
+    query = request.args.get("publication")
+    if not query or query.lower() not in RSS_FEEDS:
+        publication = "bbc"
+    else:
+        publication = query.lower()
+    feed = feedparser.parse(RSS_FEEDS[publication])
+    return render_template("home.html",
+    articles=feed['entries'])
+
+# @app.route("/bbc")
+# def bbc():
+#  return get_news('bbc')
+# @app.route("/cnn")
+# def cnn():
+#  return get_news('cnn')
 #  def get_news(publication):
 #  feed = feedparser.parse(RSS_FEEDS[publication])
 #  first_article = feed['entries'][0]
